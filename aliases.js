0dffcd8f64214ae4d589c74418022644470498f6
Travis CI User <travis@example.org>
2018-11-07 02:16:20 +0000

    Documentation for japaric/criterion.rs@87c2d3d

---
+++ aliases.js
@@ -0,0 +1,4 @@
+var ALIASES = {};
+ALIASES['criterion'] = {};
+ALIASES['criterion_plot'] = {};
+ALIASES['criterion_stats'] = {};
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["criterion_stats"] = [{text:"impl&lt;'a, A&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"criterion_stats/univariate/outliers/tukey/struct.LabeledSample.html\" title=\"struct criterion_stats::univariate::outliers::tukey::LabeledSample\">LabeledSample</a>&lt;'a, A&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: Float,&nbsp;</span>",synthetic:false,types:["criterion_stats::univariate::outliers::tukey::LabeledSample"]},{text:"impl&lt;A&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"criterion_stats/struct.Distribution.html\" title=\"struct criterion_stats::Distribution\">Distribution</a>&lt;A&gt;",synthetic:false,types:["criterion_stats::Distribution"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
